fc56e6116cda6081438ffc3c9512b62c4e0dfb47
Huynh Duc Dung <dunghd.it@gmail.com>
2021-09-11 00:47:38 +0800

    :sparkles: Add new hardhat task for mint token

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,8 +4,11 @@ import "@nomiclabs/hardhat-waffle";
 import "@openzeppelin/hardhat-upgrades";
 import "@typechain/hardhat";
 import { config } from "dotenv";
+import { readFileSync } from "fs";
 import "hardhat-gas-reporter";
 import { task } from "hardhat/config";
+import { NFTStorage, File } from "nft.storage";
+import { join } from "path";
 import "solidity-coverage";
 
 config();
@@ -20,6 +23,40 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
+
+// Mint a nft token
+task("mint", "Mint a NFT token")
+  .addParam("contract", "Contract address")
+  .addParam("owner", "Owner address")
+  .addParam("file", "FIle path")
+  .addParam("name", "File name")
+  .addParam("description", "File description")
+  .setAction(async (taskArgs, hre) => {
+    const { contract, owner, file, name, description } = taskArgs;
+    console.log(`Minting NFT token for contract ${contract}`);
+    console.log(`Owner: ${owner}`);
+    console.log(`File: ${file}`);
+    console.log(`Name: ${name}`);
+    console.log(`Description: ${description}`);
+    const token = await hre.ethers.getContractAt("MyAwesomeLogo", contract);
+
+    // upload file
+    const apiKey = process.env.NFT_STORAGE_KEY || "";
+    const client = new NFTStorage({ token: apiKey });
+    const metadata = await client.store({
+      name,
+      description,
+      image: new File([readFileSync(join(__dirname, file)).buffer], name, {
+        type: "image/png", // image/png
+      }),
+    });
+    console.log("Uploaded file to nft storage", metadata);
+
+    // mint a token
+    const tx = await token.safeMint(owner, metadata.url);
+    const { blockHash } = await tx.wait();
+    console.log("Minted NFT", blockHash);
+  });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
