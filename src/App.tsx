71dbb1f3c7b3ae106e49c88f77dc89fdfec07905
Huynh Duc Dung <dunghd.it@gmail.com>
2021-09-26 02:09:52 +0800

    :sparkles: Add NFT item form

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { NftProvider } from "use-nft";
 
 import MyAwesomeLogoArtifacts from "./artifacts/contracts/MyAwesomeLogo.sol/MyAwesomeLogo.json";
+import AddItemModal from "./components/AddItemModal";
 import Demo from "./components/Demo";
 import { getLibrary } from "./components/Demo";
 import { Nft } from "./components/Nft";
@@ -24,6 +25,7 @@ function NFTApp() {
   const { library } = useWeb3React();
   const [total, setTotal] = useState(0);
   const [page, setPage] = useState(1);
+  const [isOpen, setIsOpen] = useState(false);
   const limit = 12;
 
   useEffect(() => {
@@ -49,12 +51,38 @@ function NFTApp() {
 
   return (
     <div>
-      <h2 className="my-4 text-4xl font-bold">NFT Items</h2>
+      <h2 className="my-4 text-4xl font-bold">
+        NFT Items
+        <button
+          type="button"
+          onClick={() => setIsOpen(true)}
+          className="ml-2 btn btn-lg hover:btn-active hover:text-green-200"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          Add NFT
+        </button>
+      </h2>
+
+      <AddItemModal isOpen={isOpen} onAdd={logger.warn} onClose={() => setIsOpen(false)} />
+
       <div className="container grid gap-2 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
         {Array.from(Array(limit).keys())
           .filter((i) => i + 1 + (page - 1) * limit < total)
           .map((i) => (
-            <Nft tokenId={String(i + 1 + (page - 1) * limit)} />
+            <Nft key={i} tokenId={String(i + 1 + (page - 1) * limit)} />
           ))}
       </div>
       <Pagination currentPage={page} totalPage={Math.ceil(total / limit)} onChange={setPage} />
---
+++ src/components/AddItemModal.tsx
@@ -0,0 +1,111 @@
+import { DevTool } from "@hookform/devtools";
+import React, { useEffect, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { useForm } from "react-hook-form";
+
+type FormValues = {
+  name: string;
+  description: string;
+};
+
+type AddItemModalProps = {
+  isOpen: boolean;
+  onAdd: (formData: FormValues, files: Array<File>) => void;
+  onClose: () => void;
+};
+
+function AddItemModal({ isOpen, onAdd, onClose }: AddItemModalProps) {
+  const { register, handleSubmit, control } = useForm<FormValues>();
+
+  const [files, setFiles] = useState<
+    Array<
+      File & {
+        preview: string;
+      }
+    >
+  >([]);
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+  });
+
+  const thumbs = files.map((file) => (
+    <div className="avatar" key={file.name}>
+      <div className="w-auto h-24 mb-8 rounded-btn">
+        <img src={file.preview} alt={file.name} />
+      </div>
+    </div>
+  ));
+
+  useEffect(
+    () => () => {
+      // Make sure to revoke the data uris to avoid memory leaks
+      files.forEach((file: any) => URL.revokeObjectURL(file.preview));
+      if (files.length > 0) {
+        setFiles([]);
+      }
+    },
+    [files]
+  );
+
+  const onSubmit = handleSubmit((data) => onAdd(data, files));
+
+  return (
+    <div className={isOpen ? "modal modal-open" : "modal"}>
+      <DevTool control={control} />
+      <form onSubmit={onSubmit}>
+        <div className="modal-box">
+          <div className="p-10 card bg-base-200">
+            <section className="form-control">
+              <div {...getRootProps({ className: "container" })}>
+                <input
+                  {...getInputProps({
+                    className: "input",
+                  })}
+                />
+                <p>Drag 'n' drop some files here, or click to select files</p>
+                <em>(Only *.jpeg and *.png images will be accepted)</em>
+              </div>
+              <aside>{thumbs}</aside>
+            </section>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input type="text" placeholder="name" className="input" {...register("name")} />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Description</span>
+              </label>
+              <textarea
+                {...register("description")}
+                className="h-24 textarea textarea-bordered"
+                placeholder="Description"
+              ></textarea>
+            </div>
+          </div>
+
+          <div className="modal-action">
+            <button type="submit" className="btn btn-primary">
+              Add
+            </button>
+            <button type="button" onClick={onClose} className="btn">
+              Close
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default AddItemModal;
