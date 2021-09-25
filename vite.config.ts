28c6aa79ab9542f158e627f8d2d9fd8df986f413
Huynh Duc Dung <dunghd.it@gmail.com>
2021-09-26 02:51:37 +0800

    :sparkles: setup api proxy from web

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import checker from "vite-plugin-checker";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), checker({ typescript: true })],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
