791013470506859c2149fdd199ee4abeec430497
Huynh Duc Dung <dunghd.it@gmail.com>
2021-09-26 03:21:21 +0800

    :hammer: Add clean dist folder

--- server/src/routes/nft/index.ts
+++ server/src/routes/nft/index.ts
@@ -1,9 +1,9 @@
 import { FastifyPluginAsync } from "fastify";
 
-const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+const nft: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get("/", async function (request, reply) {
     reply.send({ hello: "world" });
   });
 };
 
-export default example;
+export default nft;
