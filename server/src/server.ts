29e74c3d5e4fccefbdbe3d4e7ee26a9f3e63c891
Huynh Duc Dung <dunghd.it@gmail.com>
2021-09-26 13:41:58 +0800

    :green_heart: try to use 3000 as default port

--- server/src/server.ts
+++ server/src/server.ts
@@ -29,7 +29,7 @@ app.addHook("onClose", async (instance, done) => {
 });
 
 // Start listening.
-app.listen(process.env.PORT || 8080, (err: any) => {
+app.listen(process.env.PORT || 3000, "0.0.0.0", (err: any) => {
   if (err) {
     app.log.error(err);
     process.exit(1);
