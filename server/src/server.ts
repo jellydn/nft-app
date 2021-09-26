653b7dce536f4809bfacc9f097c207b5327acf5b
Huynh Duc Dung <dunghd.it@gmail.com>
2021-09-26 13:14:02 +0800

    :sparkles: fastify eject with typescript

---
+++ server/src/server.ts
@@ -0,0 +1,37 @@
+// Read the .env file.
+// Require library to exit fastify process, gracefully (if possible)
+import closeWithGrace from "close-with-grace";
+import * as dotenv from "dotenv";
+// Require the framework
+import Fastify from "fastify";
+
+dotenv.config();
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: true,
+});
+
+// Register your application as a normal plugin.
+app.register(import("./app"));
+
+// delay is the number of milliseconds for the graceful close to finish
+const closeListeners = closeWithGrace({ delay: 500 }, async function (opts: any) {
+  if (opts.err) {
+    app.log.error(opts.err);
+  }
+  await app.close();
+});
+
+app.addHook("onClose", async (instance, done) => {
+  closeListeners.uninstall();
+  done();
+});
+
+// Start listening.
+app.listen(process.env.PORT || 8080, (err: any) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+});
